d6850b2248c209bf01b0c49a2bdb5b6edb904a43
DEMA1252 <80131216+DEMA1252@users.noreply.github.com>
2021-10-21 17:51:20 -0800

    Add files via upload
    vamonos]!!

---
+++ script.js
@@ -0,0 +1,95 @@
+let userScore = 0;
+let computerScore= 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result >p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const sissors_div = document.getElementById("s");
+
+function getComputerChoice (){
+  const choices = ['r', 'p','s']; 
+  const randomNumber= Math.floor(Math.random() * 3); 
+  return choices [ randomNumber ];
+}
+
+function  convertToWord (letter){
+  if (letter==="r") return "Piedra";
+  if (letter==="p") return "Papel";
+  return "Tijeras";
+}
+
+function win (userChoice,computerChoice){
+  const smallUserWord = " tu".fontsize(3).sub();
+  const smallCompWord = " maquina".fontsize(3).sub();
+ const userChoice_div = document.getElementById (userChoice);
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML =  computerScore;
+  
+  TML =result_p.innerHTML = `${convertToWord(userChoice )}${smallUserWord} mata a ${convertToWord(computerChoice )}${smallCompWord} .Tu ganas !!🔥`; 
+  userChoice_div.classList.add('green-glow');
+  setTimeout(function(){userChoice_div.classList.remove('green-glow')},1200);
+
+}
+
+function lose (userChoice,computerChoice){
+  const smallUserWord = " tu".fontsize(3).sub();
+  const smallCompWord = " maquina".fontsize(3).sub();
+  const userChoice_div = document.getElementById (userChoice);
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML =  computerScore;
+  
+  
+  TML =result_p.innerHTML = `${convertToWord(userChoice )}${smallUserWord} pierde contra ${convertToWord(computerChoice )}${smallCompWord} . Tu pierdes...🤖`; 
+   userChoice_div.classList.add('red-glow');
+  setTimeout(function(){userChoice_div.classList.remove('red-glow')},700);
+}
+function draw (userChoice,computerChoice){
+
+  const smallUserWord = " tu".fontsize(3).sub();
+  const smallCompWord = " maquina".fontsize(3).sub();
+  const userChoice_div = document.getElementById (userChoice);
+  
+  TML =result_p.innerHTML = `${convertToWord(userChoice )}${smallUserWord} es igual ${convertToWord(computerChoice )}${smallCompWord} . Es un EMPATE!`;
+   userChoice_div.classList.add('gray-glow');
+  setTimeout(function(){userChoice_div.classList.remove('gray-glow')},800);
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+switch(userChoice + computerChoice){
+  case "rs":
+  case "pr":
+  case "sp":
+   win(userChoice,computerChoice);
+    break;
+  case "rp":
+  case "ps":
+  case "sr":
+    lose(userChoice,computerChoice);
+    break;
+  case "rr":
+  case "pp":
+  case "ss":
+ draw(userChoice,computerChoice);
+    break;  
+}
+}
+
+function main(){
+ rock_div.addEventListener('click', function(){
+ game("r");
+})
+
+paper_div.addEventListener('click', function(){
+  game("p");
+  });
+
+sissors_div.addEventListener('click', function(){
+  game("s");
+ })
+}
+main ();
